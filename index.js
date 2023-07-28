a269a1f9f45699c4913e54dc5ab593bf077d6a9a
anaomart <37917804+anaomart@users.noreply.github.com>
2023-07-28 16:15:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const app = express();
+const PORT = 4000;
+const productsModel = require('./products.model');
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
+app.post('/storeProducts', async(req, res) => {
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+    try {
+        let products = await productsModel.create({
+            name: req.body.name,
+            products: req.body.products
+        })
+    } catch (err) {
+        console.log(err)
+    }
 })
+app.get('/getAll', async(req, res) => {
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    try {
+        const products = await productsModel.find()
+
+        res.json(products)
+    } catch (err) {
+        console.log(err)
+    }
+})
+app.get('/getProduct/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await productsModel.findById(id)
+
+        res.json(product)
+    } catch (err) {
+        console.log(err)
+    }
+})
+app.delete('/deleteProduct/:id', async(req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await productsModel.deleteOne({ _id: id })
+
+        res.json(product)
+    } catch (err) {
+        console.log(err)
+    }
 })
